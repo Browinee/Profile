6362a850eae5b0e8d1e27ca8da7cf4e03ec65bbf
justin.lin <justin.lin@yxunistar.com>
2021-09-13 16:16:53 +0800

    feat: add localstorage

--- src/infra/storage.ts
+++ src/infra/storage.ts
@@ -1,9 +1,12 @@
+export const LOCAL_STORAGE_PREFIX = "__auth_provider_token__";
+
+export const USER_INFO = `${LOCAL_STORAGE_PREFIX}_USER_INFO`;
 export default class Storage {
   static save<T>(key: string, value: T, expires = false) {
     window.localStorage.localStorage.setItem(key, value);
   }
 
-  static load<T>(key: string) {
+  static load(key: string) {
     return window.localStorage.getItem(key);
   }
 
