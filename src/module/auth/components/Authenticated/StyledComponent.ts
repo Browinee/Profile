76ed03d530dbb8e436bd0409b8d607ab029e8456
justin.lin <justin.lin@yxunistar.com>
2021-09-13 16:39:45 +0800

    chore comment redundant code on authenticated

--- src/module/auth/components/Authenticated/StyledComponent.ts
+++ src/module/auth/components/Authenticated/StyledComponent.ts
@@ -1,20 +1,21 @@
-import styled from "@emotion/styled";
-import Row from "../../../../components/Row";
+// import Row from "../../../../components/Row";
 
-export const Container = styled.div`
-  display: grid;
-  grid-template-rows: 6rem 1fr;
-  height: 100vh;
-`;
+import styled from "styled-components";
 
-export const Header = styled(Row)`
-  padding: 3.2rem;
-  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
-  z-index: 1;
-`;
-export const HeaderLeft = styled(Row)``;
-export const HeaderRight = styled.div``;
-export const Main = styled.main`
-  display: flex;
-  overflow: hidden;
-`;
+// export const Container = styled.div`
+//   display: grid;
+//   grid-template-rows: 6rem 1fr;
+//   height: 100vh;
+// `;
+//
+// export const Header = styled(Row)`
+//   padding: 3.2rem;
+//   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+//   z-index: 1;
+// `;
+// export const HeaderLeft = styled(Row)``;
+// export const HeaderRight = styled.div``;
+// export const Main = styled.main`
+//   display: flex;
+//   overflow: hidden;
+// `;
